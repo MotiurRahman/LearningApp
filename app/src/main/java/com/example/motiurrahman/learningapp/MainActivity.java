5633d5a7c5eee055d988fcc0ce8d6a145012e200
Motiur Rahman <motiurrahman@Motiurs-MacBook-Pro.local>
2015-07-12 01:35:17 +0600

    First commit

---
+++ app/src/main/java/com/example/motiurrahman/learningapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.motiurrahman.learningapp;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    Button btn1,btn2,btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
